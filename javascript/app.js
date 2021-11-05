c6b1ffee1e273543c537d8d45dca3690f7a625f5
Pablo Marino <pmarino2013@gmail.com>
2021-11-04 22:59:20 -0300

    Remover e insertar nodo en ejemplo

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,3 @@
-
 // let segundero=0
 // let inicio;
 
@@ -19,33 +18,34 @@
 // location.replace('https://google.com')
 // },3000)
 
-let titulo=document.querySelector('h1')
-let parrafito=document.querySelector('#parrafo')
-let textInput=document.querySelector('#inputText')
+let titulo = document.querySelector("h1");
+let parrafito = document.querySelector("#parrafo");
+let textInput = document.querySelector("#inputText");
+
+let boton = document.querySelector("button");
 
 // parrafito.innerHTML='Párrafo dinámico'
 
-let texto='Nota: del ejemplo anterior; El super()método se refiere a la clase principal. Al llamar al super()método en el método constructor, llamamos al método constructor del padre y obtenemos acceso a las propiedades y métodos del padre.'
+let texto =
+  "Nota: del ejemplo anterior; El super()método se refiere a la clase principal. Al llamar al super()método en el método constructor, llamamos al método constructor del padre y obtenemos acceso a las propiedades y métodos del padre.";
 
-const mostrarText=()=>{
+const mostrarText = () => {
+  document.body.insertBefore(parrafito, boton); //para agregarlo nuevamente si fue removido antes del nodo boton
+  parrafito.innerHTML = texto;
+};
 
-  parrafito.innerHTML=texto
+const ocultar = () => {
+  document.body.removeChild(parrafito); //Elimina el nodo
+  // parrafito.innerHTML=""
+};
 
-}
-
-const ocultar=()=>{
-  parrafito.innerHTML=""
-}
-
-const cambiarTitulo=()=>{
-  if(textInput.value===''){
-    titulo.innerText='Hola mundo'
-  }else{
-
-    titulo.innerText=textInput.value
+const cambiarTitulo = () => {
+  if (textInput.value === "") {
+    titulo.innerText = "Hola mundo";
+  } else {
+    titulo.innerText = textInput.value;
   }
-}
+};
 
 // textInput.addEventListener('change',cambiarTitulo)
-textInput.addEventListener('keyup',cambiarTitulo)
-
+textInput.addEventListener("keyup", cambiarTitulo);
